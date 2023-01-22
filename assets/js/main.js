101b82409a230e950d2937c502595579268f5c13
Ayoub Ouabi <ayoub.ouabi06@gmail.com>
2023-01-22 01:27:37 +0100

    add login page frontEnd and backend

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,31 @@ let menuButton = document.getElementById("menu-toggle");
 menuButton.onclick = function () {
     wrapper.classList.toggle("toggled");
 };
+//login partie
+function openModl(){
+    $('#modalArticle').modal('show');
+}
+function checkLogin(){
+    if(!($("#inputEmail").val()=='')  && !($("#inputPassword").val()=='')){
+        $.post("http://localhost/PlanetDEV/Controller/CheckLogin.php", {email:$("#inputEmail").val(),password:$("#inputPassword").val()},
+            function (data) {
+
+                if(data==="true"){
+                    console.log("true");
+                    location.href='http://localhost/PlanetDEV/index.php'
+                }else{
+                    $('#checkLabel').html("Email or password incorrect")
+                    $('#checkLabel').removeClass('d-none');
+                }
+            }
+        ).fail(function(data){
+            console.log(data);
+        });
+    }else{
+        $('#checkLabel').html("Email or password can't be empty")
+        $('#checkLabel').removeClass('d-none');
+    }
+
+
+}
+
